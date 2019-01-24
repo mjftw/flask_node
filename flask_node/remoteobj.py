a1780674253ea2f6b13e644bf489d6cbba19389d
Merlin Webster <mjftwebster@gmail.com>
2019-01-24 19:33:24 +0000

    Added TxClassLooping class, fixed err on api call to unknown function

--- flask_node/remoteobj.py
+++ flask_node/remoteobj.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
 import inspect
 import requests
-
+from copy import copy
+from threading import Thread, Event
+import time
 
 class TxClass():
-    def __init__(self, name=None, host='localhost', port='5000', debug=False):
+    def __init__(self, name=None, host=None, port=None, debug=None):
         name = name or '{}_api'.format(self.__class__.__name__)
         self.app = Flask(name)
         self.host = host or 'localhost'
         self.port = port or '5000'
-        self.debug = debug
+        self.debug = debug or False
 
         self.start_time = None
         if not hasattr(self, 'methods'):
@@ -34,14 +36,20 @@ class TxClass():
 
         @self.app.route('/<m>', methods=['GET'])
         def api_discover(m):
-            func = getattr(self, m)
+            func = getattr(self, m, None)
+            if not func:
+                return '', 404
+
             al = inspect.getargspec(func).args[1:] or []
             if request.method == 'GET':
                 return jsonify(get_method_call(m))
 
         @self.app.route('/<m>', methods=['POST'])
         def api_call(m):
-            func = getattr(self, m)
+            func = getattr(self, m, None)
+            if not func:
+                return '', 404
+
             al = inspect.getargspec(func).args[1:] or []
             dl = inspect.getargspec(func).defaults or []
             args_defs = [l for l in al[:len(al)-len(dl)]] + list(zip(al[::-1], dl[::-1]))[::-1]
@@ -59,12 +67,92 @@ class TxClass():
         self.app.run(host=self.host, port=self.port, debug=self.debug)
 
 
+class TxClassLooping(TxClass):
+    def __init__(self, loop_method_name, loop_sleep=1, pause_sleep=None,
+            name=None, host=None, port=None, debug=None):
+        if not hasattr(self, loop_method_name):
+            raise TypeError('Class is does not have loop method {}'.format(
+                loop_method))
+        self._loop_method_name = loop_method_name
+
+        self._loop_sleep = loop_sleep
+        self._pause_sleep = pause_sleep
+        self._stop_event = Event()
+        self._pause_event = Event()
+
+        self._loop_thread = None
+
+        super().__init__(name, host, port, debug)
+
+    def run_api(self):
+        self.start()
+        super().run_api()
+
+    def pause(self):
+        self._pause_event.set()
+
+    def resume(self):
+        self._pause_event.clear()
+
+    def stop(self):
+        print("STOP")
+        if self._loop_thread and self._loop_thread.isAlive():
+            self._stop_event.set()
+            self._loop_thread.join()
+            self._loop_thread = None
+            print("STOPPED")
+
+    def start(self):
+        print("START")
+        if not self._loop_thread or not self._loop_thread.isAlive():
+            self._loop_thread = self._thread_factory()
+            self._loop_thread.start()
+            print("STARTED")
+
+    def get_running_state(self):
+        if self._stop_event.is_set():
+            return 'stopped'
+        elif self._pause_event.is_set():
+            return 'paused'
+        else:
+            return 'running'
+
+    def _thread_factory(self):
+        return TxClassLoopThread(
+            txclass=self,
+            loop_method_name=self._loop_method_name,
+            stop_event=self._stop_event,
+            pause_event=self._pause_event,
+            loop_sleep=self._loop_sleep,
+            pause_sleep=self._pause_sleep
+        )
+
+class TxClassLoopThread(Thread):
+    def __init__(self, txclass, loop_method_name, stop_event, pause_event,
+            loop_sleep=None, pause_sleep=None):
+        self.txclass = txclass
+        self.loop_method_name = loop_method_name
+        self.stop_event = stop_event
+        self.pause_event = pause_event
+        self.loop_sleep = loop_sleep
+        self.pause_sleep = pause_sleep or 0.1
+
+        Thread.__init__(self)
+
+    def run(self):
+        loop_method = getattr(self.txclass, self.loop_method_name)
+        while not self.stop_event.is_set():
+            if self.pause_event.is_set():
+                time.sleep(self.pause_sleep)
+            else:
+                loop_method()
+                time.sleep(self.loop_sleep or 0)
+
+
 class RxClass():
     def __init__(self, host='localhost', port='5000'):
         self.host = host
         self.port = port
-
-        self.pull_methods()
 
     @property
     def url(self):
